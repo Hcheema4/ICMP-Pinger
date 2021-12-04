73c0cdd4a549d3baee30c4ccaf4cde139b842578
Hcheema4 <90523906+Hcheema4@users.noreply.github.com>
2021-12-03 20:27:16 -0500

    Add files via upload

---
+++ solution_pinger.py
@@ -0,0 +1,140 @@
+from socket import *
+import os
+import sys
+import struct
+import time
+import select
+import binascii
+# Should use stdev
+
+ICMP_ECHO_REQUEST = 8
+
+
+def checksum(string):
+    csum = 0
+    countTo = (len(string) // 2) * 2
+    count = 0
+
+    while count < countTo:
+        thisVal = (string[count + 1]) * 256 + (string[count])
+        csum += thisVal
+        csum &= 0xffffffff
+        count += 2
+
+    if countTo < len(string):
+        csum += (string[len(string) - 1])
+        csum &= 0xffffffff
+
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
+
+
+
+def receiveOnePing(mySocket, ID, timeout, destAddr):
+    timeLeft = timeout
+
+    while 1:
+        startedSelect = time.time()
+        whatReady = select.select([mySocket], [], [], timeLeft)
+        howLongInSelect = (time.time() - startedSelect)
+        if whatReady[0] == []:  # Timeout
+            return "Request timed out."
+
+        timeReceived = time.time()
+        recPacket, addr = mySocket.recvfrom(1024)
+
+        # Fill in start
+        
+        # Fetch the ICMP header from the IP packet
+        icmpHeader = recPacket[20:28]
+        
+        #ICMP header packed in the order as the sendOneping
+        icmpType, icmpCode, icmpChecksum, icmpID, icmpSequence = struct.unpack("bbHHh", icmpHeader) 
+        if type != 8 and icmpID == ID and icmpCode == 0: 
+            bytesInDouble = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+            return timeReceived - timeSent
+            
+        timeLeft = timeLeft - howLongInSelect
+         
+        # Fill in end
+        
+        if timeLeft <= 0:
+            return "Request timed out."
+
+
+def sendOnePing(mySocket, destAddr, ID):
+    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+
+    myChecksum = 0
+    # Make a dummy header with a 0 checksum
+    # struct -- Interpret strings as packed binary data
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    data = struct.pack("d", time.time())
+    # Calculate the checksum on the data and the dummy header.
+    myChecksum = checksum(header + data)
+
+    # Get the right checksum, and put in the header
+
+    if sys.platform == 'darwin':
+        # Convert 16-bit integers from host to network  byte order
+        myChecksum = htons(myChecksum) & 0xffff
+    else:
+        myChecksum = htons(myChecksum)
+
+
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    packet = header + data
+
+    mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
+
+
+    # Both LISTS and TUPLES consist of a number of objects
+    # which can be referenced by their position number within the object.
+
+def doOnePing(destAddr, timeout):
+    icmp = getprotobyname("icmp")
+
+
+    # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
+    mySocket = socket(AF_INET, SOCK_RAW, icmp)
+
+    myID = os.getpid() & 0xFFFF  # Return the current process i
+    sendOnePing(mySocket, destAddr, myID)
+    delay = receiveOnePing(mySocket, myID, timeout, destAddr)
+    mySocket.close()
+    return delay
+
+
+def ping(host, timeout=1):
+    # timeout=1 means: If one second goes by without a reply from the server,  	# the client assumes that either the client's ping or the server's pong is lost
+    dest = gethostbyname(host)
+    print("Pinging " + dest + " using Python:")
+    print("")
+    
+    icmp_array = []
+    # Send ping requests to a server separated by approximately one second
+    for i in range(0,4):
+        delay = doOnePing(dest, timeout)
+        icmp_array.append(delay)
+        print(delay)
+        time.sleep(1)  # one second
+    # Calculate vars values and return them
+    icmp_min = 1000 * min(icmp_array) #1000 is to convert seconds into mili
+    icmp_max = 1000 * max(icmp_array)
+    icmp_avg = 1000 *(sum(icmp_array) / len(icmp_array))
+    stdev_var = 1000 * statistics.stdev(icmp_array)
+    print("('" + str(round(icmp_min, 2)) + "' , '" + str(round(icmp_max, 2)) + "' , '" + str(round(icmp_avg,2)) + "' , '" + str(round(stdev_var,2)) + "')")
+    
+    vars = [str(round(icmp_min, 2)), str(round(icmp_avg, 2)), str(round(icmp_max, 2)),str(round(stdev(stdev_var), 2))]
+    
+    for i in range(0,4):
+        print(vars[i])
+    return vars
+
+if __name__ == '__main__':
+    ping("google.com")
